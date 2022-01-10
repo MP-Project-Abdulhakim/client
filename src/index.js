de9baa8a5f19c47ff44605ac7f185cf2175c4891
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2022-01-10 16:43:54 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import store from "./reducers";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./styles/index.css";
-
-
-
-
-
-
 
 ReactDOM.render(
   <Provider store={store}>
